132e9509bcd1b0a6b29ff4483ad9f6dc9b621f16
Manohar <venkatesh.manohar@sap.com>
2020-12-03 02:16:39 +0530

    mongodb changes

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const localUri = 'mongodb://localhost:27017/money_tracker';
-mongoose.connect(localUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: false }
+//const uri = 'mongodb://localhost:27017/money_tracker';
+const uri = "mongodb+srv://venkat:Initial@123@cluster0.xff4z.mongodb.net/money_tracker?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: false }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
