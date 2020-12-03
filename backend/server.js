b66cbc8ecbbe6381fe1628bb7c5c2d25070dc0f4
Manohar <venkatesh.manohar@sap.com>
2020-12-03 20:30:35 +0530

    remove proxy

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//const uri = 'mongodb://localhost:27017/money_tracker';
-const uri = process.env.MONGODB_URI || "mongodb+srv://venkat:Initial@123@cluster0.xff4z.mongodb.net/money_tracker?retryWrites=true&w=majority";
+const uri = 'mongodb://localhost:27017/money_tracker';
+// const uri = process.env.MONGODB_URI || "mongodb+srv://venkat:Initial@123@cluster0.xff4z.mongodb.net/money_tracker?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: false }
 );
 const connection = mongoose.connection;
