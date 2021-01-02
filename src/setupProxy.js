2caca080c0f99e235c2a5b6fbea449fefc91a2c7
Venkatesh Manohar <mvenkatesh238@gmail.com>
2021-01-02 18:25:22 +0530

    changing proxy target to heroku

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/transactionList", "/userAccount", "/user"], {
-      //target: "https://money-tracker-services.herokuapp.com",
-      target: "http://localhost:5000/",
+      target: "https://money-tracker-services.herokuapp.com",
+     // target: "http://localhost:5000/",
       changeOrigin: true
     })
   );
